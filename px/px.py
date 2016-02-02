68f8a0ec3043f4aeb8690a8eacf80393e68f9083
Johan Walles <walles@spotify.com>
2016-02-02 16:33:12 +0100

    Print info about all processes

--- px/px.py
+++ px/px.py
@@ -2,4 +2,21 @@
 
 import psutil
 
-print "CPU loads: " + str(psutil.cpu_times())
+# List all processes, and print for each process: PID, owner,
+# memory usage (in %), used CPU time and full command line
+#
+# FIXME: Print in columns
+#
+# FIXME: Sort printed list by (memory usage) * (used CPU time)
+for proc in psutil.process_iter():
+    try:
+        pinfo = proc.as_dict(attrs=[
+            'pid',
+            'username',
+            'memory_percent',
+            'cpu_times',
+            'cmdline'])
+    except psutil.NoSuchProcess:
+        pass
+    else:
+        print(pinfo)
