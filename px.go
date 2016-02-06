6086ba3aa0de569d886a2c7dd267bad8dc266cea
Johan Walles <johan.walles@gmail.com>
2016-02-06 13:26:22 +0100

    List all PIDs

--- px.go
+++ px.go
@@ -3,15 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/process"
 )
 
 func main() {
-	v, _ := mem.VirtualMemory()
+	pids, _ := process.Pids()
 
-	// almost every return value is a struct
-	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
-
-	// convert to JSON. String() is also implemented
-	fmt.Println(v)
+	fmt.Println(pids)
 }
