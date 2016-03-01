1202cc4948975c72345c6d80314898385f6a6899
Johan Walles <johan.walles@gmail.com>
2016-03-01 20:07:08 +0100

    Test PxProcess creation

--- px/px_process_test.py
+++ px/px_process_test.py
@@ -1,1 +1,17 @@
-assert True
+import px_process
+
+
+def test_create_process():
+    process_builder = px_process.PxProcessBuilder()
+    process_builder.pid = 7
+    process_builder.username = "usernamex"
+    process_builder.cpu_time = 1.3
+    process_builder.memory_percent = 42.7
+    process_builder.cmdline = "hej kontinent"
+    test_me = px_process.PxProcess(process_builder)
+
+    assert test_me.pid == 7
+    assert test_me.user == "usernamex"
+    assert test_me.cpu_time_s == "1.300s"
+    assert test_me.memory_percent_s == "43%"
+    assert test_me.cmdline == "hej kontinent"
