5e9c694ba7f658e09fbe830f50e0ebc5b3e96717
Johan Walles <johan.walles@gmail.com>
2016-04-03 13:14:24 +0200

    Add test for still logged in users

--- px/px_loginhistory_test.py
+++ px/px_loginhistory_test.py
@@ -14,31 +14,47 @@ def get_users_at(last_string, now, testtime):
 
 def test_get_users_at_range():
     # Test user logged in between two timestamps
+    now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
+    lastline = "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)"
+
+    # Before
     assert not get_users_at(
-        "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)",
-        datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal()),
+        lastline, now,
         datetime.datetime(2016, 03, 31, 14, 38, tzinfo=dateutil.tz.tzlocal()))
+
+    # During
     assert set(["johan"]) == get_users_at(
-        "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)",
-        datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal()),
+        lastline, now,
         datetime.datetime(2016, 03, 31, 14, 39, tzinfo=dateutil.tz.tzlocal()))
     assert set(["johan"]) == get_users_at(
-        "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)",
-        datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal()),
+        lastline, now,
         datetime.datetime(2016, 03, 31, 17, 46, tzinfo=dateutil.tz.tzlocal()))
     assert set(["johan"]) == get_users_at(
-        "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)",
-        datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal()),
+        lastline, now,
         datetime.datetime(2016, 04, 01, 11, 8, tzinfo=dateutil.tz.tzlocal()))
+
+    # After
     assert not get_users_at(
-        "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)",
-        datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal()),
+        lastline, now,
         datetime.datetime(2016, 04, 01, 11, 9, tzinfo=dateutil.tz.tzlocal()))
 
 
 def test_get_users_at_still_logged_in():
-    # FIXME: Test user still logged in
-    pass
+    now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
+    lastline = "johan     ttys000                   Sun Apr  3 11:54   still logged in"
+
+    # Before
+    assert not get_users_at(
+        lastline, now,
+        datetime.datetime(2016, 04, 03, 11, 53, tzinfo=dateutil.tz.tzlocal()))
+
+    # During
+    assert set(["johan"]) == get_users_at(
+        lastline, now,
+        datetime.datetime(2016, 04, 03, 11, 54, tzinfo=dateutil.tz.tzlocal()))
+    assert set(["johan"]) == get_users_at(
+        lastline, now,
+        datetime.datetime.now(dateutil.tz.tzlocal()))
 
 
 def test_get_users_at_remote():
