8c159ee5fa6aa1d10cef2268a373b90f6cb72896
Johan Walles <johan.walles@gmail.com>
2016-04-04 14:24:28 +0200

    Create regexp to match at least some last lines

--- px/px_loginhistory.py
+++ px/px_loginhistory.py
@@ -1,3 +1,30 @@
+import sys
+
+import re
+
+USERNAME_PART = "([^ ]+)"
+DEVICE_PART = "([^ ]+)"
+ADDRESS_PART = "([^ ]+)?"
+FROM_PART = "(.*)"
+DASH_PART = " . "
+TO_PART = "(.*)"
+DURATION_PART = "([0-9+:]+)"
+LAST_RE = re.compile(
+  USERNAME_PART +
+  " +" +
+  DEVICE_PART +
+  " +" +
+  ADDRESS_PART +
+  " +" +
+  FROM_PART +
+  DASH_PART +
+  TO_PART +
+  " *\(" +
+  DURATION_PART +
+  "\)"
+)
+
+
 def get_users_at(timestamp, last_output=None, now=None):
     """
     Return a set of strings corresponding to which users were logged in from
@@ -9,4 +36,20 @@ def get_users_at(timestamp, last_output=None, now=None):
     Optional argument now is the current timestamp for parsing last_output. Will
     be taken from the system clock if not provided.
     """
-    return None
+    users = set()
+    for line in last_output.splitlines():
+        match = LAST_RE.match(line)
+        if not match:
+            sys.stderr.write(
+                "WARNING: Please report unmatched last line at {}: <{}>\n".format(
+                    "https://github.com/walles/px/issues/new", line))
+            continue
+
+        # username = match.group(1)
+        # device = match.group(2)
+        # address = match.group(3)
+        # from_s = match.group(4)
+        # to_s = match.group(5)
+        # duration_s = match.group(6)
+
+    return users
