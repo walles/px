7e80cfd3fe874da77ec21409ac0a23e35f5f8021
Johan Walles <johan.walles@gmail.com>
2016-04-04 14:36:18 +0200

    Add debug printouts of last line grouping

--- px/px_loginhistory.py
+++ px/px_loginhistory.py
@@ -2,13 +2,15 @@ import sys
 
 import re
 
+# last regexp parts
 USERNAME_PART = "([^ ]+)"
 DEVICE_PART = "([^ ]+)"
 ADDRESS_PART = "([^ ]+)?"
-FROM_PART = "(.*)"
+FROM_PART = "(... ... .. ..:..)"
 DASH_PART = " . "
-TO_PART = "(.*)"
+TO_PART = "(..:..)"
 DURATION_PART = "([0-9+:]+)"
+
 LAST_RE = re.compile(
   USERNAME_PART +
   " +" +
@@ -45,11 +47,14 @@ def get_users_at(timestamp, last_output=None, now=None):
                     "https://github.com/walles/px/issues/new", line))
             continue
 
-        # username = match.group(1)
-        # device = match.group(2)
-        # address = match.group(3)
-        # from_s = match.group(4)
-        # to_s = match.group(5)
-        # duration_s = match.group(6)
+        username = match.group(1)
+        device = match.group(2)
+        address = match.group(3)
+        from_s = match.group(4)
+        to_s = match.group(5)
+        duration_s = match.group(6)
+        print("FIXME: Remove this printout: u={}, d={}, a={}, f={}, t={}, d={}".format(
+            username, device, address, from_s, to_s, duration_s
+        ))
 
     return users
