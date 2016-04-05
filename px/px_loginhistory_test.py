111796cfeb401707814f28478e1b2c375accae60
Johan Walles <johan.walles@gmail.com>
2016-04-05 11:20:27 +0200

    Verify no stderr printouts after any tests

--- px/px_loginhistory_test.py
+++ px/px_loginhistory_test.py
@@ -1,7 +1,16 @@
 import datetime
 
+import pytest
 import dateutil.tz
 import px_loginhistory
+
+
+@pytest.yield_fixture
+def check_output(capfd):
+    yield None
+
+    out, err = capfd.readouterr()
+    assert not err
 
 
 def get_users_at(last_output, now, testtime):
@@ -13,7 +22,7 @@ def get_users_at(last_output, now, testtime):
     return px_loginhistory.get_users_at(testtime, last_output=last_output, now=now)
 
 
-def test_get_users_at_range():
+def test_get_users_at_range(check_output):
     # Test user logged in between two timestamps
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "johan     ttys000                   Thu Mar 31 14:39 - 11:08  (20:29)"
@@ -40,7 +49,7 @@ def test_get_users_at_range():
         datetime.datetime(2016, 04, 01, 11, 9, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_still_logged_in():
+def test_get_users_at_still_logged_in(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "johan     ttys000                   Sun Apr  3 11:54   still logged in"
 
@@ -58,7 +67,7 @@ def test_get_users_at_still_logged_in():
         datetime.datetime.now(dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_remote():
+def test_get_users_at_remote(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "root     pts/1        10.1.6.120       Tue Jan 28 05:59   still logged in"
 
@@ -67,7 +76,7 @@ def test_get_users_at_remote():
         datetime.datetime.now(dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_local_osx():
+def test_get_users_at_local_osx(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "johan     ttys000                   Sun Apr  3 11:54   still logged in"
 
@@ -76,7 +85,7 @@ def test_get_users_at_local_osx():
         datetime.datetime.now(dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_local_linux():
+def test_get_users_at_local_linux(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "johan    pts/2        :0               Wed Mar  9 13:25 - 13:38  (00:12)"
 
@@ -85,7 +94,7 @@ def test_get_users_at_local_linux():
         datetime.datetime(2016, 03, 9, 13, 26, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_until_crash():
+def test_get_users_at_until_crash(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "johan     ttys001                   Thu Nov 26 19:55 - crash (27+07:11)"
 
@@ -108,7 +117,7 @@ def test_get_users_at_until_crash():
         datetime.datetime(2015, 12, 26, 19, 55, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_until_shutdown_osx():
+def test_get_users_at_until_shutdown_osx(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "_mbsetupuser  console                   Mon Jan 18 20:31 - shutdown (34+01:29)"
 
@@ -131,7 +140,7 @@ def test_get_users_at_until_shutdown_osx():
         datetime.datetime(2016, 02, 28, 20, 30, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_until_shutdown_linux():
+def test_get_users_at_until_shutdown_linux(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "johan    :0           :0               Sat Mar 26 22:04 - down   (00:08)"
 
@@ -154,7 +163,7 @@ def test_get_users_at_until_shutdown_linux():
         datetime.datetime(2016, 03, 26, 22, 15, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_multiple():
+def test_get_users_at_multiple(check_output):
     # Test multiple users logged in between two timestamps
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     lastline = "\n".join([
@@ -184,7 +193,7 @@ def test_get_users_at_multiple():
         datetime.datetime(2016, 04, 01, 11, 9, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_pseudousers_osx():
+def test_get_users_at_pseudousers_osx(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
 
     # Note trailing space in test string, we get that from last on OS X 10.11.3
@@ -202,7 +211,7 @@ def test_get_users_at_pseudousers_osx():
         datetime.datetime(2015, 10, 23, 06, 49, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_pseudousers_linux():
+def test_get_users_at_pseudousers_linux(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
 
     lastline = "reboot   system boot  4.2.0-30-generic Thu Mar  3 11:19 - 13:38 (6+02:18)"
@@ -212,7 +221,7 @@ def test_get_users_at_pseudousers_linux():
         datetime.datetime(2016, 03, 03, 11, 19, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_gone_no_logout():
+def test_get_users_at_gone_no_logout(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
 
     lastline = "johan    pts/3        :0               Mon Apr  4 23:10    gone - no logout"
@@ -222,12 +231,12 @@ def test_get_users_at_gone_no_logout():
         datetime.datetime(2016, 03, 03, 11, 19, tzinfo=dateutil.tz.tzlocal()))
 
 
-def test_get_users_at_just_run_it():
+def test_get_users_at_just_run_it(check_output):
     # Just tyre kick it live wherever we happen to be. This shouldn't crash.
     px_loginhistory.get_users_at(datetime.datetime.now(dateutil.tz.tzlocal()))
 
 
-def test_to_timestamp():
+def test_to_timestamp(check_output):
     now = datetime.datetime(2016, 04, 03, 12, 8, tzinfo=dateutil.tz.tzlocal())
     expected = datetime.datetime(2016, 03, 05, 11, 19, tzinfo=dateutil.tz.tzlocal())
     assert px_loginhistory._to_timestamp("Thu Mar  5 11:19", now) == expected
@@ -241,7 +250,7 @@ def test_to_timestamp():
     assert px_loginhistory._to_timestamp("Mon Feb 29 13:19", now) == expected
 
 
-def test_to_timedelta():
+def test_to_timedelta(check_output):
     assert px_loginhistory._to_timedelta("01:29") == datetime.timedelta(0, hours=1, minutes=29)
     assert px_loginhistory._to_timedelta("4+01:29") == datetime.timedelta(4, hours=1, minutes=29)
     assert px_loginhistory._to_timedelta("34+01:29") == datetime.timedelta(34, hours=1, minutes=29)
