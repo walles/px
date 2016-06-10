1684d4e3662def9b52ddfa6cb59647a29969389f
Johan Walles <johan.walles@gmail.com>
2016-06-10 19:08:30 +0200

    Add install script
    It's designed to be run by curling the install script and passing it to bash.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+#
+# Download and install the latest release
+REPO="walles/px"
+
+# This is the download URL for the latest release
+URL=$(curl -s https://api.github.com/repos/$REPO/releases \
+  | grep browser_download_url \
+  | head -n 1 \
+  | cut -d '"' -f 4)
+
+echo "Downloading the latest release..."
+echo "  $URL"
+TEMPFILE=$(mktemp)
+curl -L -s "$URL" > $TEMPFILE
+chmod a+x $TEMPFILE
+
+echo "Installing the latest release..."
+sudo $TEMPFILE --install
+
+rm -f $TEMPFILE
+
+echo
+echo "Installation done, now run one or both of:"
+echo "  px"
+echo "  ptop"
