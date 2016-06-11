d527f4745aceb25fb31d255e4d9455f82d0f1e10
Johan Walles <johan.walles@gmail.com>
2016-06-11 23:56:39 +0200

    install.sh: Improve reviewability
    By using "install" rather than "sudo px.pex --install".

--- install.sh
+++ install.sh
@@ -16,8 +16,11 @@ curl -L -s "$URL" > $TEMPFILE
 chmod a+x $TEMPFILE
 
 echo "Installing the latest release..."
-echo "sudo px --install"
-sudo $TEMPFILE --install
+echo
+echo "sudo install px.pex /usr/local/bin/px"
+sudo install $TEMPFILE /usr/local/bin/px
+echo "sudo install px.pex /usr/local/bin/ptop"
+sudo install $TEMPFILE /usr/local/bin/ptop
 
 rm -f $TEMPFILE
 
