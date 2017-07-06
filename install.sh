a9b87f7c18629e0e16f116606c976982b02b0bda
Johan Walles <johan.walles@gmail.com>
2017-07-06 07:04:34 +0200

    Add shellcheck verification of shell scripts
    Shellcheck rules.
    
    Ref: https://www.shellcheck.net/

--- install.sh
+++ install.sh
@@ -18,17 +18,17 @@ URL=$(curl -s https://api.github.com/repos/$REPO/releases \
 echo "Downloading the latest release..."
 echo "  $URL"
 TEMPFILE=$(mktemp || mktemp -t px-install.XXXXXXXX)
-curl -L -s "$URL" > $TEMPFILE
-chmod a+x $TEMPFILE
+curl -L -s "$URL" > "$TEMPFILE"
+chmod a+x "$TEMPFILE"
 
 echo "Installing the latest release..."
 echo
 echo "sudo install px.pex /usr/local/bin/px"
-sudo install $TEMPFILE ${PXPREFIX}/px
+sudo install "$TEMPFILE" "${PXPREFIX}/px"
 echo "sudo install px.pex /usr/local/bin/ptop"
-sudo install $TEMPFILE ${PXPREFIX}/ptop
+sudo install "$TEMPFILE" "${PXPREFIX}/ptop"
 
-rm -f $TEMPFILE
+rm -f "$TEMPFILE"
 
 echo
 echo "Installation done, now run one or both of:"
