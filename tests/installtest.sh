ca82a798952883db76abf60ef75915d0cfc2b66d
Johan Walles <johan.walles@gmail.com>
2018-06-24 20:15:40 +0200

    Improve install script rate limit handling

--- tests/installtest.sh
+++ tests/installtest.sh
@@ -21,8 +21,12 @@ chmod a+x "${CURL}"
 # ... and put both first in the PATH
 export PATH=${WORKDIR}:$PATH
 
-# Verify happy path install
-PXPREFIX=${WORKDIR} bash -x ./install.sh
+cat << EOF
+#
+#  Testing happy-path install...
+#
+EOF
+PXPREFIX=${WORKDIR} bash ./install.sh
 test -x "${WORKDIR}/px"
 test -x "${WORKDIR}/ptop"
 
@@ -37,6 +41,11 @@ chmod a+x "${CURL}"
 rm "${WORKDIR}/px"
 rm "${WORKDIR}/ptop"
 
+cat << EOF
+#
+#  Testing rate-limited by Github install...
+#
+EOF
 # Installation should fail with an error code, that's why we're "!"ing it
 ! PXPREFIX=${WORKDIR} bash ./install.sh 2> "${WORKDIR}/message.txt"
 # The installer should just have printed the error message on stderr
