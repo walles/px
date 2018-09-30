965f466350c0c479378c494c7a3ecfb4d65f19d5
Josue Ortega <josue@debian.org>
2018-09-29 19:39:39 -0600

    Update tests for new CPU output

--- tests/px_process_test.py
+++ tests/px_process_test.py
@@ -98,6 +98,7 @@ def test_ps_line_to_process_3():
         "   4432"
         " Thu Feb 25 07:42:36 2016"
         " " + str(os.getuid()) +
+        " 5.5"
         "    1-19:31:31"
         " 19.7"
         " /usr/sbin/mysqld"
@@ -110,6 +111,7 @@ def test_ps_line_to_process_3():
         " --socket=/var/run/mysqld/mysqld.sock"
         " --port=3306", testutils.now())
     assert process.username == getpass.getuser()
+    assert process.cpu_usage_s == "6%"
     assert process.memory_percent_s == "20%"
     assert process.cpu_time_s == "1d19h"
     assert process.command == "mysqld"
--- tests/px_terminal_test.py
+++ tests/px_terminal_test.py
@@ -10,8 +10,8 @@ from . import testutils
 def test_to_screen_lines_unbounded():
     procs = [testutils.create_process(commandline="/usr/bin/fluff 1234")]
     assert px_terminal.to_screen_lines(procs, None) == [
-        "  PID COMMAND USERNAME   CPU RAM COMMANDLINE",
-        "47536 fluff   root     0.03s  0% /usr/bin/fluff 1234"
+        "  PID COMMAND USERNAME CPU CPU TIME RAM COMMANDLINE",
+        "47536 fluff   root      0% 0.03s  0% /usr/bin/fluff 1234"
     ]
 
 
