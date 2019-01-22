9820264d9750c339f7bf3e3407d826f8e4637ff8
Johan Walles <johan.walles@gmail.com>
2019-01-22 20:37:19 +0100

    Tune script after testing it for realz

--- update-ptop-screenshot-gif.sh
+++ update-ptop-screenshot-gif.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # See: https://sipb.mit.edu/doc/safe-shell/
-set -ef -o pipefail
+set -eu -o pipefail
 
-# FIXME: Prompt user to set a good window size to begin with
+# FIXME: Prompt user to set a good window size to begin with, 90x24 for example
 
 # FIXME: Get these some other way on Linux?
 brew install asciinema giflossy imagemagick
@@ -20,9 +20,14 @@ asciinema rec --stdin --command="./px.pex --top" --overwrite "$RECORDING"
 echo Recording saved into "$RECORDING"
 
 # Convert the recording to a gif
-./node_modules/.bin/asciicast2gif "$RECORDING" doc/ptop-screenshot.gif
+./node_modules/.bin/asciicast2gif -S1 "$RECORDING" doc/ptop-screenshot-new.gif
 
 rm "$RECORDING"
 
-# Demo what we captured
-open -a 'Google Chrome' doc/ptop-screenshot.gif
+# Demo what we captured compared to what we had
+for SCREENSHOT in doc/ptop-screenshot*.gif; do
+    echo
+    ls -l "$SCREENSHOT"
+    file "$SCREENSHOT"
+    open -a 'Google Chrome' "$SCREENSHOT"
+done
