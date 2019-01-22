ac49b852cb9518f1c0aad40e7820d4b4f7744dfe
Johan Walles <johan.walles@gmail.com>
2019-01-22 20:12:28 +0100

    Finalize recording script

--- update-ptop-screenshot-gif.sh
+++ update-ptop-screenshot-gif.sh
@@ -5,13 +5,24 @@ set -ef -o pipefail
 
 # FIXME: Prompt user to set a good window size to begin with
 
-# FIXME: Install some other way on Linux? "pip3 install"?
-brew install asciinema
+# FIXME: Get these some other way on Linux?
+brew install asciinema giflossy imagemagick
 
-# FIXME: ./ci.sh
+npm install asciicast2gif
+
+# Make sure we have the latest build before recording
+./ci.sh
+
+# Record!
 RECORDING=$(mktemp || mktemp -t ptop-ascii-recording.XXXXXXXX)
 echo Recording into: $RECORDING...
 asciinema rec --stdin --command="./px.pex --top" --overwrite $RECORDING
 echo Recording saved into $RECORDING
 
-# FIXME: Use asciicast2gif to convert the recording to a gif
+# Convert the recording to a gif
+./node_modules/.bin/asciicast2gif $RECORDING doc/ptop-screenshot.gif
+
+rm $RECORDING
+
+# Demo what we captured
+open -a 'Google Chrome' doc/ptop-screenshot.gif
