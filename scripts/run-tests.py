6a717ad756da141633fb92b799dcc165735a0484
Johan Walles <johan.walles@gmail.com>
2019-02-02 18:16:08 +0100

    Print ISO timestamps

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -3,8 +3,12 @@
 Run tests using pytest but only ones that need rerunning
 """
 
+import os
+
 import pytest
 import coverage
+import datetime
+import dateutil.tz
 
 # FIXME: Figure these out dynamically
 TEST_FILE = "tests/px_file_test.py"
@@ -17,6 +21,11 @@ pytest.main(args=[TEST_FILE + "::" + TEST_NAME])
 cov.stop()
 coverage_data = cov.get_data()
 
+# FIXME: If the test failed, drop it from the cache
+
 # Print covered files
 for file in coverage_data.measured_files():
-    print(file)
+    epoch_timestamp = os.path.getmtime(file)
+    datetime_timestamp = datetime.datetime.fromtimestamp(epoch_timestamp, dateutil.tz.tzlocal())
+
+    print(datetime_timestamp.isoformat(), " ", file)
