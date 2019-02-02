22354c884f763b90f18c8dcc2cad82e1db723c14
Johan Walles <johan.walles@gmail.com>
2019-02-02 22:43:12 +0100

    Move test running into a function

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -16,10 +16,6 @@ CACHEROOT = '.pytest-avoidance'
 # FIXME: This should be python-<version>-<hash-of-the-python-binary>
 VM_IDENTIFIER = 'python-1.2.3'
 
-# FIXME: Figure these out dynamically
-TEST_FILE = "tests/px_file_test.py"
-TEST_NAME = "test_listen_name"  # NOTE: The slow one is "test_get_all"
-
 
 # From: https://stackoverflow.com/a/600612/473672
 def mkdir_p(path):
@@ -32,33 +28,40 @@ def mkdir_p(path):
             raise
 
 
-# Run test and collect coverage data
-cov = coverage.Coverage()
-cov.start()
-exitcode = pytest.main(args=[TEST_FILE + "::" + TEST_NAME])
-if exitcode is not 0:
-    # FIXME: Remove any cached data for this test and run the next one instead
-    pass
+def run_test(test_file, test_name):
+    # FIXME: Check with cache, do we really need to run this?
 
-cov.stop()
-coverage_data = cov.get_data()
+    # Run test and collect coverage data
+    cov = coverage.Coverage()
+    cov.start()
+    exitcode = pytest.main(args=[test_file + "::" + test_name])
+    if exitcode is not 0:
+        # FIXME: Remove any cached data for this test and run the next one instead
+        pass
 
-# Store the file-timestamps list into a file
-#
-# File naming scheme:
-# .pytest-avoidance/<VM-identifier>/<path to .py file>/testname.deps
+    cov.stop()
+    coverage_data = cov.get_data()
 
-# FIXME: Ensure TEST_FILE path doesn't start with "/", or CACHEROOT and VM_IDENTIFIER
-# will be dropped by os.path.join()
-cachedir = os.path.join(CACHEROOT, VM_IDENTIFIER, TEST_FILE)
+    # Store the file-timestamps list into a file
+    #
+    # File naming scheme:
+    # .pytest-avoidance/<VM-identifier>/<path to .py file>/testname.deps
 
-print ("Cachedir name: " + cachedir)
-mkdir_p(cachedir)
-depsfile_name = os.path.join(cachedir, TEST_NAME + ".deps")
-print ("Depsfile name: " + depsfile_name)
-# Now write our stuff into depsfile
-with open(depsfile_name, "w") as depsfile:
-    for file in coverage_data.measured_files():
-        epoch_timestamp = os.path.getmtime(file)
-        datetime_timestamp = datetime.datetime.fromtimestamp(epoch_timestamp, dateutil.tz.tzlocal())
-        depsfile.write("%s %s\n" % (datetime_timestamp.isoformat(), file))
+    # FIXME: Ensure test_file path doesn't start with "/", or CACHEROOT and VM_IDENTIFIER
+    # will be dropped by os.path.join()
+    cachedir = os.path.join(CACHEROOT, VM_IDENTIFIER, test_file)
+
+    print ("Cachedir name: " + cachedir)
+    mkdir_p(cachedir)
+    depsfile_name = os.path.join(cachedir, test_name + ".deps")
+    print ("Depsfile name: " + depsfile_name)
+    # Now write our stuff into depsfile
+    with open(depsfile_name, "w") as depsfile:
+        for file in coverage_data.measured_files():
+            epoch_timestamp = os.path.getmtime(file)
+            datetime_timestamp = datetime.datetime.fromtimestamp(epoch_timestamp, dateutil.tz.tzlocal())
+            depsfile.write("%s %s\n" % (datetime_timestamp.isoformat(), file))
+
+
+# FIXME: Discover these like pytest does
+run_test("tests/px_file_test.py", "test_listen_name")
