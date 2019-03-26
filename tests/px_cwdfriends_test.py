d1d0a7ef15b63cba864bed272705bb457c38cdb2
Johan Walles <johan.walles@gmail.com>
2019-03-26 08:52:47 +0100

    Validate friends list

--- tests/px_cwdfriends_test.py
+++ tests/px_cwdfriends_test.py
@@ -24,3 +24,23 @@ def test_current_cwd_notroot():
     test_me = px_cwdfriends.PxCwdFriends(process, [process], [cwd_file])
     assert test_me.cwd == '/notroot'
     assert test_me.friends == []
+
+
+def test_find_friends():
+    process = testutils.create_process(pid=123)
+    friend = testutils.create_process(pid=234)
+    notfriend1 = testutils.create_process(pid=666)
+    notfriend2 = testutils.create_process(pid=667)
+
+    cwd_file = testutils.create_file("cwd", "/notroot", None, 123)
+    friend_cwd = testutils.create_file("cwd", "/notroot", None, 234)
+    notfriend_cwd = testutils.create_file("cwd", "/somewhereelse", None, 666)
+    # notfriend2 cwd intentionally left blank for the sake of this test
+
+    test_me = px_cwdfriends.PxCwdFriends(
+        process,
+        [process, friend, notfriend1, notfriend2],
+        [cwd_file, friend_cwd, notfriend_cwd])
+
+    assert test_me.cwd == '/notroot'
+    assert test_me.friends == [friend]
