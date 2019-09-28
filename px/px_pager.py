b2de4c9137edcc3efdd50b90eb4245e56f1a1cd3
Johan Walles <johan.walles@gmail.com>
2019-09-28 16:19:53 +0200

    If available, get pager from $PAGER

--- px/px_pager.py
+++ px/px_pager.py
@@ -7,8 +7,9 @@ from . import px_processinfo
 
 if sys.version_info.major >= 3:
     # For mypy PEP-484 static typing validation
-    from . import px_process  # NOQA
-    from typing import List   # NOQA
+    from . import px_process     # NOQA
+    from typing import List      # NOQA
+    from typing import Optional  # NOQA
 
 
 def _pump_info_to_fd(fileno, process, processes):
@@ -24,10 +25,59 @@ def _pump_info_to_fd(fileno, process, processes):
         pass
 
 
+# From: https://stackoverflow.com/a/377028/473672
+def which(program):
+    # type: (Optional[str]) -> Optional[str]
+    if not program:
+        return None
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
+def to_command_line(spec):
+    # type: (Optional[str]) -> Optional[List[str]]
+    if not spec:
+        return None
+
+    split_spec = spec.split()
+    arg0 = which(split_spec[0])
+    if not arg0:
+        # Not found
+        return None
+
+    return [arg0] + split_spec[1:]
+
+
 def launch_pager():
-    # FIXME: Get a suitable pager + command line options based on the $PAGER
-    # variable
-    return subprocess.Popen(['moar'], stdin=subprocess.PIPE)
+    env = os.environ.copy()
+
+    # Prevent --quit-if-one-screen, we always want a pager
+    env['LESS'] = ''
+
+    pager_cmd = to_command_line(env.get('PAGER', None))
+    if not pager_cmd:
+        # Prefer moar: https://github.com/walles/moar
+        pager_cmd = to_command_line('moar')
+    if not pager_cmd:
+        pager_cmd = to_command_line('less')
+
+    # FIXME: What should we do if we don't find anything to page with?
+    assert pager_cmd is not None
+
+    return subprocess.Popen(pager_cmd, stdin=subprocess.PIPE, env=env)
 
 
 def page_process_info(process, processes):
