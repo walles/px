db2fe3c37328304dd63d5aae9dcd0131173c02d2
Johan Walles <johan.walles@gmail.com>
2019-09-28 14:53:20 +0200

    Add FIXMEs

--- px/px_pager.py
+++ px/px_pager.py
@@ -12,10 +12,15 @@ if sys.version_info.major >= 3:
 def page_process_info(process, processes):
     # type: (px_process.PxProcess, List[px_process.PxProcess]) -> None
 
+    # FIXME: Get a suitable pager + command line options based on the $PAGER variable
     pager = subprocess.Popen(['moar'], stdin=subprocess.PIPE)
     pager_stdin = pager.stdin
     assert pager_stdin is not None
+
+    # FIXME: If the pager goes away before we're done with this, arrow buttons
+    # don't work any more after coming back into ptop
     px_processinfo.print_process_info(pager_stdin.fileno(), process, processes)
+
     pager_stdin.close()
 
     # FIXME: If this returns an error code, what do we do?
