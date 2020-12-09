53c46f72c53852757ddf4292efe7cc36da9df3ea
Johan Walles <johan.walles@gmail.com>
2020-12-09 19:04:14 +0100

    Test 'px 1235'

--- tests_py3/px_test.py
+++ tests_py3/px_test.py
@@ -1,6 +1,7 @@
 import sys
 
 from px import px
+from px import px_processinfo
 from unittest.mock import patch
 
 @patch('px.px.install')
@@ -32,7 +33,15 @@ def test_cmdline_version(mock):
     first_print_arg: str = mock.call_args.args[0]
     assert first_print_arg.startswith('1.')
 
-# FIXME: Test 'px 1'
+@patch("px.px_processinfo.print_pid_info")
+def test_cmdline_pid(mock):
+    px._main(['px', '1235'])
+
+    mock.assert_called_once()
+    # First arg is a file descriptor that changes on every invocation, check the
+    # PID arg only.
+    assert mock.call_args.args[1] == 1235
+
 
 # FIXME: Test 'px root'
 
