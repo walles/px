97224b362dd3b9ede493bb103ad05b97d9abc040
Johan Walles <johan.walles@gmail.com>
2020-12-09 22:04:30 +0100

    Improve CI debugging statements

--- tests_py3/px_test.py
+++ tests_py3/px_test.py
@@ -43,7 +43,7 @@ def test_cmdline_pid(mock):
     px._main(['px', '1235', '--no-pager'])
 
     mock.assert_called_once()
-    print(mock.call_args.args)
+    print(mock.call_args)
     # First arg is a file descriptor that changes on every invocation, check the
     # PID arg only.
     assert mock.call_args.args[1] == 1235
@@ -53,8 +53,8 @@ def test_cmdline_filter(mock):
     px._main(['px', 'root', '--no-pager'])
 
     mock.assert_called_once()
+    print(mock.call_args)
     processes: List[px_process.PxProcess] = mock.call_args.args[0]
-    print(processes)
 
     # This assumes something is running as root. We can assume that, right?
     assert len(processes) > 0
