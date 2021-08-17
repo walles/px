835cbd344580f01e9e9bd54a6a403c52cd0e5c24
Johan Walles <johan.walles@gmail.com>
2021-08-17 06:13:42 +0200

    Make mypy a bit happier

--- px/px_functools.py
+++ px/px_functools.py
@@ -1,5 +1,17 @@
+import sys
+
+from typing import TypeVar, Callable, Any
+if sys.version_info.major >= 3:
+    # For mypy PEP-484 static typing validation
+    from typing import Dict, Tuple, Callable  # NOQA
+
+
+F = TypeVar('F', bound=Callable[..., Any])
+
+
 def cached(size=200):
-    cache = {}
+    # type: (int) -> Callable[[F], F]
+    cache = {}  # type: Dict[Tuple, Any]
     def decorator(function):
         def wrapper(*args, **kwargs):
             args_tuple = tuple(args)
