c8dca2facdeb1ab9268c9b74ca51e4fa8eb99180
Johan Walles <johan.walles@gmail.com>
2021-12-08 06:42:01 +0100

    px+ptop: Highlight username column
    Same as the processinfo highlighting.

--- tests/px_terminal_test.py
+++ tests/px_terminal_test.py
@@ -18,7 +18,9 @@ def test_to_screen_lines_unbounded():
     procs = [testutils.create_process(commandline="/usr/bin/fluff 1234")]
     assert px_terminal.to_screen_lines(procs, None, None) == [
         "\x1b[1m  PID COMMAND USERNAME CPU CPUTIME RAM COMMANDLINE\x1b[22m",
-        "47536 fluff   root     "
+        "47536 fluff   "
+        + px_terminal.faint("root")
+        + "     "
         + px_terminal.faint(" 0%")
         + " "
         + px_terminal.bold("  0.03s")
