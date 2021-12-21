8e0dc6b50be1eb9bbd71d0d99a1db1e4161a44d5
Johan Walles <johan.walles@gmail.com>
2021-12-21 19:06:36 +0100

    Show the right processes
    In the RAM bar

--- px/px_rambar.py
+++ px/px_rambar.py
@@ -1,4 +1,5 @@
 import sys
+import operator
 
 from px import px_terminal
 
@@ -11,6 +12,8 @@ if sys.version_info.major >= 3:
     from typing import Tuple  # NOQA
     from six import text_type  # NOQA
 
+GROUPS_COUNT = 4
+
 
 def get_categories(processes):
     # type: (List[px_process.PxProcess]) -> List[Tuple[text_type, int]]
@@ -18,11 +21,33 @@ def get_categories(processes):
     Group processes by pretty names, keeping track of the total rss_kb in each
     group.
 
-    Return the top three groups in order plus one "other" which is the sum of
-    the rest.
+    Return the top groups in order plus one "other" which is the sum of the
+    rest. The total number of returned groups will be GROUPS_COUNT.
     """
 
-    return [("apa", 1000), ("bepa", 300), ("cepa", 200), ("others", 666)]
+    names_to_kilobytes = {}  # type: Dict[text_type, int]
+    for process in processes:
+        base_kb = 0
+        if process.command in names_to_kilobytes:
+            base_kb = names_to_kilobytes[process.command]
+        else:
+            base_kb = 0
+
+        names_to_kilobytes[process.command] = base_kb + process.rss_kb
+
+    sorted_names = sorted(
+        names_to_kilobytes.items(), key=operator.itemgetter(1), reverse=True
+    )
+    other_total_kb = 0
+    return_me = []  # type: List[Tuple[text_type, int]]
+    for i, name_and_kilobytes in enumerate(sorted_names):
+        if i < GROUPS_COUNT - 1:
+            return_me.append(name_and_kilobytes)
+        else:
+            other_total_kb += name_and_kilobytes[1]
+    return_me.append(("others", other_total_kb))
+
+    return return_me
 
 
 def rambar(ram_bar_length, processes):
