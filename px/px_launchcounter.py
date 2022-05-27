478330c8807abe8ab3b931fa08708df7b74f72f9
Johan Walles <johan.walles@gmail.com>
2022-05-27 19:22:39 +0200

    Fix px_launchcounter

--- px/px_launchcounter.py
+++ px/px_launchcounter.py
@@ -1,5 +1,3 @@
-import sys
-
 from . import px_terminal
 
 from . import px_process
@@ -14,14 +12,14 @@ def render_launch_tuple(launch_tuple: Tuple[str, int]) -> str:
     count = launch_tuple[1]
     if count == 0:
         return binary
-    else:
-        return px_terminal.bold(binary) + "(" + str(count) + ")"
+
+    return px_terminal.bold(binary) + "(" + str(count) + ")"
 
 
 def _get_minus_max_score(tuples_list: List[Tuple[str, int]]) -> int:
     max_score = 0
-    for tuple in tuples_list:
-        max_score = max(max_score, tuple[1])
+    for current_tuple in tuples_list:
+        max_score = max(max_score, current_tuple[1])
     return -max_score
 
 
@@ -31,6 +29,86 @@ def sort_launchers_list(
     return sorted(launchers_list, key=_get_minus_max_score)
 
 
+def _strip_parentheses(s: str) -> str:
+    if not s:
+        return s
+    if s[0] != "(":
+        return s
+    if s[-1] != ")":
+        return s
+    return s[1:-1]
+
+
+def _list_new_launches(
+    before: List[px_process.PxProcess],
+    after: List[px_process.PxProcess],
+) -> List[px_process.PxProcess]:
+    pid2oldProc: Dict[int, px_process.PxProcess] = {}
+    for old_proc in before:
+        pid2oldProc[old_proc.pid] = old_proc
+
+    new_procs = []  # List[px_process.PxProcess]
+    for new_proc in after:
+        if new_proc.pid not in pid2oldProc:
+            # This is a new process
+            new_procs.append(new_proc)
+            continue
+
+        old_proc = pid2oldProc[new_proc.pid]
+        if old_proc.start_time != new_proc.start_time:
+            # This is a new process, PID has been reused
+            new_procs.append(new_proc)
+            continue
+
+    return new_procs
+
+
+def _to_tuple_list(launcher_list: Tuple[str, ...], count: int) -> List[Tuple[str, int]]:
+    """
+    Converts from: (("a", "b", "c"), 5)
+                to  : [("a", 0), ("b", 0), ("c", 5)]
+    """
+    tuple_list: List[Tuple[str, int]] = []
+    for launcher in launcher_list:
+        tuple_list.append((launcher, 0))
+    tuple_list[-1] = (launcher_list[-1], count)
+    return tuple_list
+
+
+def _merge_tuple_lists(
+    tl1: List[Tuple[str, int]],
+    tl2: List[Tuple[str, int]],
+) -> Optional[List[Tuple[str, int]]]:
+    if len(tl1) > len(tl2):
+        longer = tl1
+        shorter = tl2
+    else:
+        longer = tl2
+        shorter = tl1
+
+    merged = longer[:]
+    for i, t1 in enumerate(shorter):
+        t2 = longer[i]
+        if t1[0] != t2[0]:
+            # Mismatch, we can't merge these
+            return None
+        merged[i] = (t1[0], t1[1] + t2[1])
+
+    return merged
+
+
+def _callchain(process: px_process.PxProcess) -> Tuple[str, ...]:
+
+    reverse_callchain: List[str] = []
+
+    current: Optional[px_process.PxProcess] = process
+    while current is not None:
+        reverse_callchain.append(_strip_parentheses(current.command))
+        current = current.parent
+
+    return tuple(reversed(reverse_callchain))
+
+
 class Launchcounter:
     def __init__(self):
         self._hierarchies: Dict[Tuple[str, ...], int] = {}
@@ -38,58 +116,13 @@ class Launchcounter:
         # Most recent process snapshot
         self._last_processlist: Optional[List[px_process.PxProcess]] = None
 
-    def _strip_parentheses(self, s: str) -> str:
-        if not s:
-            return s
-        if s[0] != "(":
-            return s
-        if s[-1] != ")":
-            return s
-        return s[1:-1]
-
-    def _callchain(self, process: px_process.PxProcess) -> Tuple[str, ...]:
-
-        reverse_callchain: List[str] = []
-
-        current: Optional[px_process.PxProcess] = process
-        while current is not None:
-            reverse_callchain.append(self._strip_parentheses(current.command))
-            current = current.parent
-
-        return tuple(reversed(reverse_callchain))
-
     def _register_launches(self, new_processes: List[px_process.PxProcess]) -> None:
-
         for new_process in new_processes:
-            callchain = self._callchain(new_process)
+            callchain = _callchain(new_process)
             if callchain in self._hierarchies:
                 self._hierarchies[callchain] += 1
             else:
                 self._hierarchies[callchain] = 1
-
-    def _list_new_launches(
-        self,
-        before: List[px_process.PxProcess],
-        after: List[px_process.PxProcess],
-    ) -> List[px_process.PxProcess]:
-        pid2oldProc: Dict[int, px_process.PxProcess] = {}
-        for old_proc in before:
-            pid2oldProc[old_proc.pid] = old_proc
-
-        new_procs = []  # List[px_process.PxProcess]
-        for new_proc in after:
-            if new_proc.pid not in pid2oldProc:
-                # This is a new process
-                new_procs.append(new_proc)
-                continue
-
-            old_proc = pid2oldProc[new_proc.pid]
-            if old_proc.start_time != new_proc.start_time:
-                # This is a new process, PID has been reused
-                new_procs.append(new_proc)
-                continue
-
-        return new_procs
 
     def update(self, procs_snapshot: List[px_process.PxProcess]) -> None:
 
@@ -97,46 +130,10 @@ class Launchcounter:
             self._last_processlist = procs_snapshot
             return
 
-        new_processes = self._list_new_launches(self._last_processlist, procs_snapshot)
+        new_processes = _list_new_launches(self._last_processlist, procs_snapshot)
         self._register_launches(new_processes)
 
         self._last_processlist = procs_snapshot
-
-    def _to_tuple_list(
-        self, launcher_list: Tuple[str, ...], count: int
-    ) -> List[Tuple[str, int]]:
-        """
-        Converts from: (("a", "b", "c"), 5)
-                 to  : [("a", 0), ("b", 0), ("c", 5)]
-        """
-        tuple_list: List[Tuple[str, int]] = []
-        for launcher in launcher_list:
-            tuple_list.append((launcher, 0))
-        tuple_list[-1] = (launcher_list[-1], count)
-        return tuple_list
-
-    def _merge_tuple_lists(
-        self,
-        tl1: List[Tuple[str, int]],
-        tl2: List[Tuple[str, int]],
-    ) -> Optional[List[Tuple[str, int]]]:
-        if len(tl1) > len(tl2):
-            longer = tl1
-            shorter = tl2
-        else:
-            longer = tl2
-            shorter = tl1
-
-        merged = longer[:]
-        for i in range(0, len(shorter)):
-            t1 = shorter[i]
-            t2 = longer[i]
-            if t1[0] != t2[0]:
-                # Mismatch, we can't merge these
-                return None
-            merged[i] = (t1[0], t1[1] + t2[1])
-
-        return merged
 
     def _coalesce_launchers(self) -> List[List[Tuple[str, int]]]:
         coalesced: List[List[Tuple[str, int]]] = []
@@ -144,12 +141,12 @@ class Launchcounter:
         for launcher_list in sorted(self._hierarchies.keys()):
             count = self._hierarchies[launcher_list]
 
-            new_tuple_list = self._to_tuple_list(launcher_list, count)
+            new_tuple_list = _to_tuple_list(launcher_list, count)
             if len(coalesced) == 0:
                 coalesced.append(new_tuple_list)
                 continue
 
-            merged = self._merge_tuple_lists(coalesced[-1], new_tuple_list)
+            merged = _merge_tuple_lists(coalesced[-1], new_tuple_list)
             if merged:
                 coalesced[-1] = merged
             else:
