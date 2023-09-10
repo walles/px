13c6416c10e50098252d90a0bba4a33cf039efb6
Johan Walles <johan.walles@gmail.com>
2023-09-10 19:36:01 +0200

    Speed up local development scripts

--- pxtree.sh
+++ pxtree.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Run ptop from current sources
+# Run pxtree from current sources
 
-tox -e package >/dev/null && ./px.pex --tree "$@"
+PYTHONPATH=px:$(echo env/lib/python*/site-packages) python3 -m px.px --tree "$@"
