aa5602810706e3032931a449b92f3d5a1917d8d2
Johan Walles <johan.walles@gmail.com>
2024-02-26 07:16:46 +0100

    Reinstate the old spaced command lines tests
    Since we can now deal with having multiple spaces in the binaries and
    their paths again.

--- tests/px_commandline_test.py
+++ tests/px_commandline_test.py
@@ -379,23 +379,23 @@ def test_get_command_sudo():
 
 def test_get_command_sudo_with_space_in_command_name(tmpdir):
     # Create a file name with a space in it
-    spaced_path = tmpdir.join("with space")
+    spaced_path = tmpdir.join("i contain spaces")
     spaced_path.write_binary(b"")
     spaced_name = str(spaced_path)
 
     # Verify splitting of the spaced file name
-    assert px_commandline.get_command("sudo " + spaced_name) == "sudo with space"
+    assert px_commandline.get_command("sudo " + spaced_name) == "sudo i contain spaces"
 
     # Verify splitting with more parameters on the line
     assert (
         px_commandline.get_command("sudo " + spaced_name + " parameter")
-        == "sudo with space"
+        == "sudo i contain spaces"
     )
 
 
 def test_get_command_sudo_with_space_in_path(tmpdir):
     # Create a file name with a space in it
-    spaced_dir = tmpdir.join("with space")
+    spaced_dir = tmpdir.join("i contain spaces")
     os.mkdir(str(spaced_dir))
     spaced_path = spaced_dir + "/runme"
 
